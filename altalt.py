1214282181ae06abf505fca9e8cbedd3b9b18ab3
Liamw <l0i4a1m5@gmail.com>
2025-06-16 22:45:30 +0900

    Branch first commit

--- altalt.py
+++
@@ -1,8 +0,0 @@
-code_lines = ["once()", "printv()", "repeat()", "a = 8", "if ()", "endloop","endif","end"]
-
-endindexcount = 0
-def find_end(cmdindex):
-    endindex = (cmdindex + 1) * -1
-    return endindex
-
-print(code_lines[find_end(0)], find_end(0))
